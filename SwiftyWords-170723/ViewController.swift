efe3f123629ff938879b341cd62bf1db309b7566
Joshisaka1004 <joshi1004@gmail.com>
2017-08-07 23:22:44 +0200

    Just another test

--- SwiftyWords-170723/ViewController.swift
+++ SwiftyWords-170723/ViewController.swift
@@ -13,11 +13,17 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var cluesLabel: UILabel!
     
+    @IBOutlet weak var myStackView: UIStackView!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var currentAnswer: UITextField!
     @IBOutlet weak var answersLabel: UILabel!
-    var score = 0
-    var level = 2
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+        
+    }
+    var level = 1
     var myWordButtons = [UIButton]()
     var activatedButtons = [UIButton]()
     var solutions = [String]()
@@ -25,10 +31,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for eachSubview in view.subviews where eachSubview.tag == 1001 {
-            let buttons = eachSubview as! UIButton
-            myWordButtons.append(buttons)
-            buttons.addTarget(self, action: #selector(changeBackground), for: .touchUpInside)
+        for i in myStackView.subviews {
+            for j in i.subviews {
+                let buttons = j as! UIButton
+                myWordButtons.append(buttons)
+                buttons.addTarget(self, action: #selector(changeBackground), for: .touchUpInside)
+            }
         }
         loadLevel()
     }
@@ -40,22 +48,65 @@ class ViewController: UIViewController {
         // (1) Wir durchsuchen den Hauptpfad unserer App (Bundle ist die Gesamtheit aller mit unserer App verbundenen Daten/Dateien) nach einer Datei namens Level xxx (xxx=Integer) vom Datentyp txt. Wir prüfen also, ob es im gesamten App-Bundle eine Datei namens Levelxxx.txt existiert, wobei xxx für eine Zahl vom Typ Integer steht, die den jeweiligen Level spezifiziert.
         if let levelPath = Bundle.main.path(forResource: "level\(level)", ofType: "txt") {
             // (2) Wenn oben diese Datei gefunden wurde, versuchen wir, aus dem GESAMTEN Inhalt dieser (Text-)Datei einen einzigen String zu machen. Aus diesem String machen wir schließlich einen Array von Substrings, die Trennung des Strings in Substrings erfolgt gemäß eines Separators, in unserem Fall ist dies die neue Zeile.
-            let inhalt = try? String(contentsOfFile: levelPath)
-            let gesamtInhalt = inhalt!.components(separatedBy: "\n")
-            print(gesamtInhalt)
+            if let inhalt = try? String(contentsOfFile: levelPath) {
+                let gesamtInhalt = inhalt.components(separatedBy: "\n")
+                let myShuffling = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: gesamtInhalt) as! [String]
+                for (index,val) in myShuffling.enumerated() {
+                    let temp1 = val.components(separatedBy: ": ")
+                    cluesString += "\(index + 1). \(temp1[1])\n"
+                    let temp2 = temp1[0].replacingOccurrences(of: "|", with: "")
+                    solutionString += "\(temp2.count) letters\n"
+                    solutions.append(temp2)
+                    let teile = temp1[0].components(separatedBy: "|")
+                    wortFragmente += teile
+                }
+                //print(wortFragmente)
+            }
         }
-    }
-    
-    @objc func changeBackground(buttons: UIButton) {
-        view.backgroundColor = UIColor.yellow
-    }
-    
-    @IBAction func submitTapped(_ sender: UIButton) {
+        //print("www \(solutionString)")
+        cluesLabel.text = cluesString.trimmingCharacters(in: .whitespacesAndNewlines)
+        answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+        let shuffledWortFragmente = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: wortFragmente) as! [String]
+        if myWordButtons.count == wortFragmente.count {
+            for j in 0..<myWordButtons.count {
+                myWordButtons[j].setTitle("\(shuffledWortFragmente[j])", for: .normal)
+            }
+        }
         
     }
     
-    @IBAction func clearTapped(_ sender: UIButton) {
-      
+    
+    @objc func changeBackground(buttons: UIButton) {
+        if currentAnswer.text != nil, let hisCurrentAnswer = buttons.titleLabel!.text {
+            currentAnswer.text = currentAnswer.text! + hisCurrentAnswer
+            activatedButtons.append(buttons)
+            buttons.isHidden = true
+        }
     }
+    
+    @IBAction func submitTapped(_ sender: UIButton) {
+        if let solutionPosition = solutions.index(of: "\(currentAnswer.text!)") {
+            activatedButtons.removeAll()
+            var splitClues = answersLabel.text!.components(separatedBy: "\n")
+            splitClues[solutionPosition] = "\(currentAnswer.text!)"
+            answersLabel.text = splitClues.joined(separator: "\n")
+            score += 1
+            //scoreLabel.text = "Score: \(score)"
+            currentAnswer.text = ""
+        }
+        if score % 7 == 0 && score > 0 {
+            let myAC = UIAlertController(title: "SOLVED", message: "All levels over", preferredStyle: .alert)
+            myAC.addAction(UIAlertAction(title: "OK!", style: .default, handler: nil))
+            present(myAC, animated:  true)
+        }
+    }
+    
+    @IBAction func clearTapped(_ sender: UIButton) {
+        currentAnswer.text = ""
+        for j in 0..<activatedButtons.count {
+            activatedButtons[j].isHidden = false
+        }
+        activatedButtons.removeAll()
+    }
+  
 }
-
--- SwiftyWords-170723/myTestVC.swift
+++ SwiftyWords-170723/myTestVC.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class myTestVC: UIViewController {
 
+    @IBOutlet weak var myPuzzleLabel: UILabel!
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let myPuzzles = Bundle.main.path(forResource: "PuzzlesText", ofType: "txt") {
+            if let myPuzzlesText = try? String(contentsOfFile: myPuzzles) {
+                let finalPuzzleText = myPuzzlesText.components(separatedBy: "\n")
+                myPuzzleLabel.text = myPuzzlesText
+                //let joinedText = finalPuzzleText.joined(separator: "\n")
+                //myPuzzleLabel.text = joinedText
+                print(finalPuzzleText)
+                labelColor()
+            }
+        }
 
     }
 
+    @IBAction func changeTextButton(_ sender: Any) {
+        myLabel.text = "Wow, I've been changed!"
+        let myAC = UIAlertController(title: "Begrüßung", message: "Hallo Jochen", preferredStyle: .alert)
+        present(myAC, animated: false)
+        myAC.addAction(UIAlertAction(title: "OK!", style: .cancel, handler: nil))
+    }
+    func labelColor() {
+        let randomA:CGFloat = CGFloat(arc4random_uniform(256))
+        let randomB:CGFloat = CGFloat(arc4random_uniform(256))
+        let randomC:CGFloat = CGFloat(arc4random_uniform(256))
+        myPuzzleLabel.backgroundColor = UIColor(red: randomA/255, green: randomB/255, blue: randomC/255, alpha: 1.0)
+        
+    }
+   
 }
